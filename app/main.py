4f2e9ae69dc648b8fbe3793d7189e6bcb128076f
p4n <ibrahimdogan.js@gmail.com>
2026-02-05 19:03:38 +0300

    feat(builtins): implement 'type' command

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import sys
-
-
 def main():
+    builtinList = ['exit', 'echo', 'type']
     while True:
         sys.stdout.write("$ ")
         command = input()
@@ -11,6 +10,11 @@ def main():
             break
         if command.split()[0] == "echo":
             print(command[5:])
+        elif command.split()[0] == "type":
+            if command.split()[1] in builtinList:
+                print(f'{command.split()[1]} is a shell builtin')
+            else:
+                print(f'{command.split()[1]}: not found')
         else:
             print(f"{command}: command not found")
 
