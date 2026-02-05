0493d81a835a0a43fb4d5a4d6ea0db6969e29079
p4n <ibrahimdogan.js@gmail.com>
2026-02-05 18:47:47 +0300

    feat(builtins): implement 'echo' command
    added support for 'echo' to print arguments to standard output.

--- app/main.py
+++ app/main.py
@@ -5,9 +5,14 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
+        if not command:
+            continue
         if command == "exit":
             break
-        print(f"{command}: command not found")
+        if command.split()[0] == "echo":
+            print(command[5:])
+        else:
+            print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
