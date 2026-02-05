b66b4d234c2c77e3f4b5866a71d409bac8f2cf9a
p4n <ibrahimdogan.js@gmail.com>
2026-02-05 18:32:23 +0300

    feat(builtins): implement 'exit' command
    added support for 'exit' to break the REPL loop and terminate the program gracefully.

--- app/main.py
+++ app/main.py
@@ -5,8 +5,9 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
+        if command == "exit":
+            break
         print(f"{command}: command not found")
-    pass
 
 
 if __name__ == "__main__":
